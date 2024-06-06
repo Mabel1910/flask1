65d3c3815e03651a2aaf1546db8f68ddbca33f04
Mabel <mabelbaam19@gmail.com>
2024-06-06 11:00:05 -0600

    tarea actualizada 3

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-productos = []
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        nombre_producto = request.form['nombre']
-        precio_producto = request.form['precio']
-        productos.append({'nombre': nombre_producto, 'precio': precio_producto})
-        return redirect(url_for('index'))
-    return render_template('index.html', productos=productos)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
