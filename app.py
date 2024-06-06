013faca65805001453715fddbbf8299625837523
Mabel <mabelbaam19@gmail.com>
2024-06-06 10:41:01 -0600

    tarea actualizada

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-#importar 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-#crear una app 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hola mundo"
+productos = []
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nombre_producto = request.form['nombre']
+        precio_producto = request.form['precio']
+        productos.append({'nombre': nombre_producto, 'precio': precio_producto})
+        return redirect(url_for('index'))
+    return render_template('index.html', productos=productos)
 
-#mandamos a llamar al objetos y lo corremos con run 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     app.run(debug=True)
